760d434e92a4427f7d35986715aa3700d30f6aff
Muhammad Wajahat Tariq <wajahatariq4@gmail.com>
2025-11-20 05:21:09 +0500

    Update app.js

--- app/frontend/app.js
+++ app/frontend/app.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "https://twhspec.onrender.com";
-const WS_URL = "wss://twhspec.onrender.com/ws/manager";
+const API_BASE_URL = "https://twhspec-dk49.onrender.com";
+const WS_URL = "wss://twhspec-dk49.onrender.com/ws/manager";
  // use wss:// in production
 
 let authToken = null;
@@ -1190,4 +1190,5 @@ function init() {
 }
 
 document.addEventListener("DOMContentLoaded", init);
+
 
